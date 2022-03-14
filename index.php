2744f55913383cdd0321aacc1540fdbf85e25727
muhamad rafli <muhamadrafli6207@gmail.com>
2022-03-14 11:11:02 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,207 @@
+<div class="header-banner">
+<?php
+    include 'template/header.php'
+?>
+<div class="banner-text">
+    <div class="banner-content">
+        <h1>Go scale up <br>for your business</h1>
+        <p>Transaksi digital yang memberikan solusi untuk meningkatkan penjualan bisnis anda.</p>
+        <div class="button-layout">
+            <a href="#"><button>Get started</button></a>
+            <div class="content-banner-data">
+                <h2>1800+</h2>
+                <span>Merchent Project</span>
+            </div>
+            <div class="content-banner-data">
+                <h2>6K</h2>
+                <span>Customer</span>
+            </div>
+        </div>
+    </div>
+    <div class="banner-image">
+        <img src="asset/banner-image.png" alt="">
+        <img src="asset/chart.png" alt="" class="chart">
+        <img src="asset/chart-outline.png" alt="" class="chart-outline">
+        <img src="asset/parcel.png" alt="" class="parcel">
+    </div>
+    </div>
+</div>
+
+<div class="section-2">
+    <div class="running-text">
+        <div class="text">Get promo for merchant 70%</div>
+        <div class="text">Ramadhan sale get 50%</div>
+        <div class="text">Free fee transaction</div>
+    </div>
+</div>
+
+<div class="section-3">
+    <div class="section-3-image">
+        <img src="asset/image-2.png" alt="">
+    </div>
+    <div class="section-3-text">
+        <h1>Registration Now!</h1>
+        <div class="divider"></div>
+        <div class="section-3-button">
+            <a href="sign-up.php"><button>Coba gratis</button></a>
+        </div>
+        <span>Term & Condition</span>
+    </div>
+</div>
+
+<div class="online-mobile">
+    <div class="online-mobile-content">
+        <h1>POSe point of sale</h1>
+        <p>Menghadirkan platform digital selling
+ yang menjaga keamanan data dan solusi bisnis</p>
+        <img src="asset/chart-line.png" alt="">
+    </div>
+    <div class="online-mobile-image">
+        <img src="asset/phone-max.png" alt="">
+    </div>
+</div>
+
+<div class="tentang-pose">
+    <div class="tentang-pose-image">
+        <img src="asset/tentang-pose.png" alt="">
+    </div>
+    <div class="tentang-pose-content">
+        <h1>Tentang POSe</h1>
+        <div class="divider" style="margin:0;"></div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quidem aliquam molestiae quaerat neque, soluta repellendus. Quidem tempora consectetur eius neque. Perspiciatis, necessitatibus voluptatum corrupti delectus tempora ipsa maxime voluptates quibusdam sint at quasi expedita ratione quo consequatur culpa eius, in aperiam obcaecati cumque laborum? Impedit esse cumque officia expedita!</p>
+        <a href="#"><button>See More</button></a>
+    </div>
+</div>
+
+<div class="instruksi">
+    <h1>Kemudahan Menggunakan Sistem</h1>
+    <div class="divider" style="width:12%;"></div>
+    <p>Bantu kembangkan usaha dan bisnis anda dengan hanya satu 
+aplikasi saja, fitur lengkap, mudah, dan nyaman!</p>
+    <div class="instruksi-box">
+        <div class="instruksi-card">
+            <div class="instruksi-image">
+                <img src="asset/emoney.png" alt="">
+            </div>
+            <p>E Money System</p>
+        </div>
+        <div class="instruksi-card">
+            <div class="instruksi-image">
+                <img src="asset/qr.png" alt="">
+            </div>
+            <p>QR Code</p>
+        </div>
+        <div class="instruksi-card">
+            <div class="instruksi-image">
+                <img src="asset/24.png" alt="">
+            </div>
+            <p>Layanan Service 24 Jam</p>
+        </div>
+        <div class="instruksi-card">
+            <div class="instruksi-image">
+                <img src="asset/foods.png" alt="">
+            </div>
+            <p>Available For All Merchane</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="swiper mySwiper">
+<h1>Berita</h1>
+<div class="divider" style="width:12%; margin-bottom:3.5vw;"></div>
+      <div class="swiper-wrapper berita-wrapper">
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-4.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-3.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-2.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-1.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-4.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="berita-box">
+                <div class="berita-image">
+                    <img src="asset/blog-4.png" alt="">
+                </div>
+                <div class="berita-content">
+                        <span>ADMIN, CATEGORY - NOV 16, 2022</span>
+                        <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat varius pretium mattis quis vitae in in.</p>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination "></div>
+</div>
+
+<div class="partnership-homepage">
+    <h1>Partnership</h1>
+    <div class="divider" style="width:12%;"></div>
+    <div class="partnership">
+        <div class="logo-partnership"><img src="asset/bank-bjb.png" alt=""></div>
+        <div class="logo-partnership"><img src="asset/dtpeduli.png" alt=""></div>
+    </div>
+</div>
+
+
+<?php
+    include 'template/footer.php'
+?>
+    
+
+
